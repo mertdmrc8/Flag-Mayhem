aac666a6b8e071b01c402f463ce08f660fc46757
mertdmrc8 <mertdemirci@trakya.edu.tr>
2022-07-19 09:55:37 +0300

    hareket

--- My project/Assets/Script/Character_Controller.cs
+++ My project/Assets/Script/Character_Controller.cs
@@ -9,7 +9,7 @@ public class Character_Controller : MonoBehaviour
     Rigidbody2D rb;
 
     public float speed = 5f;
-    public float jumpSpeed = 3f;
+    public float jumpSpeed = 5f;
     private float direction = 0f;
 
     
@@ -17,6 +17,10 @@ public class Character_Controller : MonoBehaviour
     float groundcheckRadius = 0.1f;
     public LayerMask groundLayer;
     public Transform groundcheck;
+
+
+    private float vertical;
+    private float horizontal;
 
     PhotonView pw;
     void Start()
@@ -28,10 +32,14 @@ public class Character_Controller : MonoBehaviour
 
         if (pw.IsMine == false)
         {
-            rb=GetComponent<Rigidbody2D>();
-            GetComponent<SpriteRenderer>().color = Color.red;
-                          
            
+            GetComponent<SpriteRenderer>().color = Color.red;
+
+
+        }
+        else if (pw.IsMine)
+        {
+            rb = GetComponent<Rigidbody2D>();
         }
     }
 
@@ -42,32 +50,39 @@ public class Character_Controller : MonoBehaviour
         {
             isTouchingGround = Physics2D.OverlapCircle(groundcheck.position, groundcheckRadius, groundLayer);
             direction = Input.GetAxis("Horizontal");
-            //Movement();
+            Movement();
         }
     }
-    //private void Movement()
-    //{
-    //    if (direction > 0f)
-    //    {
-    //        rb.velocity = new Vector2(direction * speed, rb.velocity.y);
-    //    }
-    //    else if (direction < 0f)
-    //    {
-    //        rb.velocity = new Vector2(direction * speed, rb.velocity.y);
-            
-    //    }
-    //    else
-    //    {
-    //        rb.velocity = new Vector2(0, rb.velocity.y);
-    //    }
+    private void Movement()
+    {
+        if (direction > 0f)
+        {
+            rb.velocity = new Vector2(direction * speed, rb.velocity.y);
+        }
+        else if (direction < 0f)
+        {
+            rb.velocity = new Vector2(direction * speed, rb.velocity.y);
 
-    //    if (Input.GetButtonDown("Jump") && isTouchingGround)
-    //    {
-    //        rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
-    //    }
-    //}
+        }
+        else
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+
+        if (Input.GetButtonDown("Jump") && isTouchingGround)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+        }
+
+
+
+
+
+
+    }
     private void FixedUpdate()
     {
        
+        
     }
 }
