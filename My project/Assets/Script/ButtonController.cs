4ee655b48f00cd6e7a0395a42e70b61cbb2a471d
mertdmrc8 <mertdemirci@trakya.edu.tr>
2022-07-20 14:32:13 +0300

    Squashed commit of the following:
    commit 4beb370501aec15263a504b1bdd9851fe59a6639
    Author: mertdmrc8 <mertdemirci@trakya.edu.tr>
    Date:   Wed Jul 20 14:24:54 2022 +0300
    
        14.24
    
    commit 69e2a255b4939e908be398baaabf6e41fc1cd77d
    Author: mertdmrc8 <mertdemirci@trakya.edu.tr>
    Date:   Wed Jul 20 11:57:27 2022 +0300
    
        11:57
    
    commit 8cd23281329f00b56ef48f05c117db6152b7eff1
    Author: mertdmrc8 <mertdemirci@trakya.edu.tr>
    Date:   Wed Jul 20 11:36:09 2022 +0300
    
        11:36
    
    commit 2ed92d1a0835cb301aa0567b07c1f7c38dd1deca
    Author: mertdmrc8 <mertdemirci@trakya.edu.tr>
    Date:   Wed Jul 20 07:33:06 2022 +0300
    
        shoot
    
    commit 8f70cae3f241ea9dc0887a47e65ab0a85d08a573
    Author: mertdmrc8 <mertdemirci@trakya.edu.tr>
    Date:   Tue Jul 19 13:58:52 2022 +0300
    
        animasyon düzeltme
    
    commit 3c7de2541e4310dc8587aedbf02cf92cd9cd72be
    Author: mertdmrc8 <mertdemirci@trakya.edu.tr>
    Date:   Tue Jul 19 13:38:09 2022 +0300
    
        ş
    
    commit b5fffadb88f923b8228faabb8f17888bf99c425f
    Author: mertdmrc8 <mertdemirci@trakya.edu.tr>
    Date:   Tue Jul 19 13:18:22 2022 +0300
    
        ğğ
    
    commit f3f78ed34c26d23ba2cf6addbf3ed3c7d001e558
    Author: mertdmrc8 <mertdemirci@trakya.edu.tr>
    Date:   Tue Jul 19 13:10:03 2022 +0300
    
        as
    
        as
    
    commit 9860258fadd1c7718c177ead2a317dd362c64601
    Merge: aac666a c58b064
    Author: mertdmrc8 <mertdemirci@trakya.edu.tr>
    Date:   Tue Jul 19 12:55:30 2022 +0300
    
        Merge branch 'main' into branch2
    
    commit aac666a6b8e071b01c402f463ce08f660fc46757
    Author: mertdmrc8 <mertdemirci@trakya.edu.tr>
    Date:   Tue Jul 19 09:55:37 2022 +0300
    
        hareket

--- My project/Assets/Script/ButtonController.cs
+++ My project/Assets/Script/ButtonController.cs
@@ -2,17 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine; 
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
+
 public class ButtonController : MonoBehaviour
 { 
  
-
-    [SerializeField]
-    private GameObject authenticationPrefab; 
-
-     [SerializeField]
-    private GameObject Canvals ; 
-     
     void Start()
     {
         
@@ -31,54 +24,4 @@ public class ButtonController : MonoBehaviour
     public void quitbutton(){
       Application.Quit();
     }
-
-    public void loginbutton(){ 
-    Authentication();
-
-
-    }
-
-    public void signinbutton(){  
-    Authentication();
-    }  
-
-
-  //   public void connection_button_onclick(){ 
-
-  //     }
-
-  //    public void exit_button_onclick(){ 
-
-  // }
-    
-  private void Authentication(){
-
-    GameObject cloneprefab= Instantiate(authenticationPrefab, new Vector3(Canvals.transform.position.x, Canvals.transform.position.y, Canvals.transform.position.z), Quaternion.identity);
-     
-    cloneprefab.transform.eulerAngles=  new Vector3(cloneprefab.transform.eulerAngles.x,cloneprefab.transform.eulerAngles.y,cloneprefab.transform.eulerAngles.z-90f);
-    
-    
-    
-    cloneprefab.transform.parent = Canvals.transform;   
-
-    Button connection_button =cloneprefab.transform.GetChild(0).GetComponent<Button>(); 
-    Button exit_button  =     cloneprefab.transform.GetChild(1).GetComponent<Button>();
-
-    connection_button.onClick.AddListener(delegate { 
-      
-
-       });
-
-    exit_button.onClick.AddListener(delegate { 
-       Destroy(cloneprefab);
-
-
-       });
-  }
-
-
-
-
-
-
 }
