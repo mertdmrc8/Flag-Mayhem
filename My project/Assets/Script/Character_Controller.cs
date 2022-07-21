539cb637bb07303d38aad20df4b3216c101c731b
mertdmrc8 <mertdemirci@trakya.edu.tr>
2022-07-21 11:48:17 +0300

    11:48

--- My project/Assets/Script/Character_Controller.cs
+++ My project/Assets/Script/Character_Controller.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Realtime;
 using Photon.Pun;
-public class Character_Controller : MonoBehaviour
+using System;
+
+public class Character_Controller : MonoBehaviourPun, IPunObservable
 {
     public Animator anim;
     Rigidbody2D rb;
@@ -12,21 +14,25 @@ public class Character_Controller : MonoBehaviour
     public float speed = 7f;
     public float jumpSpeed = 5f;
     private float direction = 0f;
+    private Vector2 smootMove;
 
-    
+    private GameObject  playerCamera;
+    private GameObject sceneCamera;
     private bool isTouchingGround;
     float groundcheckRadius = 0.1f;
     public LayerMask groundLayer;
     public Transform groundcheck;
 
-    [HideInInspector] public bool isFacingRight=true;
+    [HideInInspector] public bool isFacingRight = true;
 
     PhotonView pw;
     void Start()
     {
 
+        sceneCamera = GameObject.Find("Main Camera");
+        playerCamera = GameObject.Find("PlayerCamera");
 
-        pw=GetComponent<PhotonView>();
+        pw = GetComponent<PhotonView>();
 
 
         if (pw.IsMine == false)
@@ -38,21 +44,27 @@ public class Character_Controller : MonoBehaviour
         }
         else if (GetComponent<PhotonView>().IsMine == true)
         {
-            rb=GetComponent<Rigidbody2D>();
+            rb = GetComponent<Rigidbody2D>();
             anim.SetBool("isWalking", false);
-            
+
         }
-         
-        
+
+        //if (pw.IsMine)
+        //{
+        //    sceneCamera.SetActive(false);
+        //    playerCamera.SetActive(true);
+        //}
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (pw.IsMine==true)
+        if (pw.IsMine)
         {
             isTouchingGround = Physics2D.OverlapCircle(groundcheck.position, groundcheckRadius, groundLayer);
-            
+
             Movement();
 
             //Animation
@@ -60,7 +72,7 @@ public class Character_Controller : MonoBehaviour
             {
                 anim.SetBool("isWalking", true);
             }
-            else 
+            else
             {
                 anim.SetBool("isWalking", false);
             }
@@ -70,41 +82,63 @@ public class Character_Controller : MonoBehaviour
             }
 
         }
+        else if (!pw.IsMine)
+        {
+            SmoothMovement();
+        }
     }
+
+    private void SmoothMovement()
+    {
+        transform.position = Vector2.Lerp(transform.position, smootMove, Time.deltaTime * 10);
+    }
+
     private void Movement()
     {
         direction = Input.GetAxisRaw("Horizontal");
         if (direction > 0f)
         {
             rb.velocity = new Vector2(direction * speed, rb.velocity.y);
-            pw.transform.eulerAngles = new Vector3(0, 0, 0); // Flipped
+            transform.eulerAngles = new Vector3(0, 0, 0); // Flipped
             isFacingRight = true;
-            
-          
+
+
         }
         else if (direction < 0f)
         {
             rb.velocity = new Vector2(direction * speed, rb.velocity.y);
-            pw.transform.eulerAngles = new Vector3(0, 180, 0); // Flipped
+            transform.eulerAngles = new Vector3(0, 180, 0); // Flipped
             isFacingRight = false;
-            
+
         }
-        
+
         else
         {
             rb.velocity = new Vector2(0, rb.velocity.y);
 
         }
-      
+
 
         if (Input.GetButtonDown("Jump") && isTouchingGround)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
-            
+
         }
     }
     private void FixedUpdate()
     {
         Debug.Log(direction);
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(transform.position);
+        }
+        else if (stream.IsReading)
+        {
+            smootMove = (Vector2)stream.ReceiveNext();
+        }
     }
 }
