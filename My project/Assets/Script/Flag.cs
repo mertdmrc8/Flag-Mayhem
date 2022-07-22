f093eded46b1afb3203bd308a7434cb83820803a
mertdmrc8 <mertdemirci@trakya.edu.tr>
2022-07-22 11:25:50 +0300

    22.07   11:25

---
+++ My project/Assets/Script/Flag.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Flag : MonoBehaviour
+{
+    PhotonView pw;
+
+ 
+
+    private void Start()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject Player = null;
+        if (collision.tag == "Player")
+        {
+            Player = collision.gameObject;
+            
+            GameObject Flag = GameObject.Find("Flag");
+        
+            this.transform.parent = Player.transform;
+
+        }
+
+    }
+}
